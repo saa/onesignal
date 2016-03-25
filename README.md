9ceded17b68a7f89e3b648d366a7781b36283946
Sergey Abramyan <sa.abramyan@gmail.com>
2016-03-25 12:50:39 +0300

    Change API and update README

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 onesignal
-=====
+=========
 
-An OTP library
+Erlang OneSignal API http://onesignal.com
 
 Build
 -----
 
     $ rebar3 compile
+
+Description
+-----------
+
+All methods accept params like OneSignal API for example [Create notification](https://documentation.onesignal.com/docs/notifications-create-notification)
+
+Status
+------
+
+* Create player
+* Send notification
--- src/onesignal.erl
+++ src/onesignal.erl
@@ -1,8 +1,8 @@
 -module('onesignal').
 
 %% API exports
--export([create_player/3, create_player/4]).
--export([send_notification/1]).
+-export([create_player/1, create_player/2]).
+-export([send_notification/2]).
 
 -define(API_BASE, <<"https://onesignal.com/api/v1">>).
 
@@ -10,31 +10,27 @@
 %% API functions
 %%====================================================================
 
-create_player(AppId, DeviceType, Identifier) ->
-    create_player(AppId, DeviceType, Identifier, #{}).
-
-create_player(AppId, DeviceType, Identifier, Params) when is_map(Params) ->
-    Params2 = Params#{device_type => device_type_to_code(DeviceType),
-                      app_id => AppId,
-                      identifier => Identifier
-                     },
+create_player(DeviceType, Params) when is_binary(DeviceType), is_map(Params) ->
+    Params2 = Params#{device_type => device_type_to_code(DeviceType)},
     create_player(Params2).
 
 create_player(Params) when is_map(Params) ->
-    case request(post, <<"players">>, Params) of
+    case request(post, <<"/players">>, Params) of
         {ok, 200, _RespHeaders, Ref} ->
-            JSON = hackney:body(Ref),
+            {ok, JSON} = hackney:body(Ref),
             {ok, jsx:decode(JSON, [return_maps])};
-        {ok, StatusCode, _RespHeaders, _Ref} ->
-            {error, StatusCode}
+        {ok, StatusCode, _RespHeaders, Ref} ->
+            {ok, ErrorBody} = hackney:body(Ref),
+            {error, StatusCode, ErrorBody}
     end.
 
-send_notification(Params) ->
-    case request(post, <<"/notifications">>, Params) of
+send_notification(ApiKey, Params) ->
+    case request(post, <<"/notifications">>, ApiKey, Params) of
         {ok, 200, _RespHeaders, _Ref} ->
             ok;
-        {ok, StatusCode, _RespHeaders, _Ref} ->
-            {error, StatusCode}
+        {ok, StatusCode, _RespHeaders, Ref} ->
+            {ok, ErrorBody} = hackney:body(Ref),
+            {error, StatusCode, ErrorBody}
     end.
 
 %%====================================================================
@@ -46,23 +42,25 @@ request(Method, Path, Params) ->
     JSON = jsx:encode(Params),
     hackney:request(Method, URL, headers(), JSON).
 
+request(Method, Path, ApiKey, Params) ->
+    URL = <<?API_BASE/binary, Path/binary>>,
+    JSON = jsx:encode(Params),
+    hackney:request(Method, URL, headers(ApiKey), JSON).
+
 headers() ->
     [{<<"content-type">>, <<"application/json">>}].
 
-%% headers(ApiKey) when is_list(ApiKey) ->
-%%     headers(list_to_binary(ApiKey));
-%% headers(ApiKey) when is_binary(ApiKey) ->
-%%     [{<<"Authorization">>, <<"Basic ", ApiKey/binary>>}].
+headers(ApiKey) when is_list(ApiKey) ->
+    headers(list_to_binary(ApiKey));
+headers(ApiKey) when is_binary(ApiKey) ->
+    [{<<"Authorization">>, <<"Basic ", ApiKey/binary>>} | headers()].
 
-%% prepare_identifier(Identifier) when is_binary(Identifier) ->
-%%     binary:replace(Identifier, <<"-">>, <<"">>, [global]).
-
-device_type_to_code(ios) -> 0;
-device_type_to_code(android) -> 1;
-device_type_to_code(amazon) -> 2;
-device_type_to_code(windowsphone_mpns) -> 3;
-device_type_to_code(chrome_app) -> 4;
-device_type_to_code(chrome_website) -> 5;
-device_type_to_code(windowsphone_wns) -> 6;
-device_type_to_code(safari) -> 7;
-device_type_to_code(firefox) -> 8.
+device_type_to_code(<<"ios">>) -> 0;
+device_type_to_code(<<"android">>) -> 1;
+device_type_to_code(<<"amazon">>) -> 2;
+device_type_to_code(<<"windowsphone_mpns">>) -> 3;
+device_type_to_code(<<"chrome_app">>) -> 4;
+device_type_to_code(<<"chrome_website">>) -> 5;
+device_type_to_code(<<"windowsphone_wns">>) -> 6;
+device_type_to_code(<<"safari">>) -> 7;
+device_type_to_code(<<"firefox">>) -> 8.
